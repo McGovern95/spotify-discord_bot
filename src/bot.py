428f768150023301f816c5f14c179308e73dcb84
Christian McGovern <mcgovernchristian@gmail.com>
2020-07-10 16:54:06 -0600

    connection made

--- src/bot.py
+++ src/bot.py
@@ -1,1 +1,22 @@
-import spotipy
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+SERVER = os.getenv('DISCORD_SERVER')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for server in client.guilds:
+        if server.name == SERVER:
+            break
+
+    print(f'{client.user} has connected to Server: {server.name}\n')
+    print('Peeps:\n {}'.format(' - '.join([member.name for member in server.members])))
+    
+
+
+client.run(TOKEN)
